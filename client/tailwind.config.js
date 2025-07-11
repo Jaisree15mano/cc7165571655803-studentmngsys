c40fc675895bfb51b93fd2babd9c7b241e49a7a2
charulatha0517 <charulatha9489@gmail.com>
2025-07-11 12:50:53 +0530

    initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+import { plugin } from "postcss";
+
+export default{
+    content:[
+        "./index.html","./src/**/*.{js,jsx,ts,tsk}",
+    ],
+    theme:{
+        extent:{},
+    },
+    plugins:[],
+}
